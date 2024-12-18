ca4c13b235b437b587ba431dcc77e017bfe8a3f4
Anjihee <jihee5100@sookmyung.ac.kr>
2024-12-18 17:03:14 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by 안지희 on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
